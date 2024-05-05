1858e17b0fa5359223face1b152cad3e12c4bccd
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-05-04 21:48:32 -0500

    03 variables

---
+++ c/02_commenst.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	printf("two types of comments // and /* */"); // this is a comment 
+	/* the code below will print the words bla blaA
+	 *
+	 * */
+	printf("bla bla hellow world");
+	printf("bla bla hellow world");
+	
+	return 0;
+}
---
+++ c/03_variables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	printf("There are different types of variables: \n");
+	printf("int -> stores integers \n");
+	printf("float -> stores floating point numbers \n");
+	printf("char -> stores single characters (Characters are surrounded by single quotes) \n");
+	printf("int -> stores integers \n");
+
+	printf(" SYNTAX: ");
+	printf(" type variable = value  ");
+
+	int myNum = 0;
+	float myF = 23.45;
+	char letra = 'D' ;
+
+	
+	
+
+	return 0;
+}
+
