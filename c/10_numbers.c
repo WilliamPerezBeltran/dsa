781c4d12a86b7db2c89be1271f294ef9afaa9edb
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-05-08 04:43:27 -0500

    add numbers

---
+++ c/10_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	/* Numeric data types
+	 *     Data      size            precision
+	 *     int        2 or 4  bytes      ---
+	 *     float      2 bytes            6 or 7
+	 *     double     4 bytes            15 
+	 *
+	 *     precesion: The precision of a floating point value
+	 *     indicats how many digits the value can have after the 
+	 *     decimal point.
+	 */
+
+	float f1 = 25e3;
+	double d1 = 12E4;
+
+	printf("%f\n", f1);
+	printf("%fl\n", d1);
+
+	return 1;
+}
