21e8d6a6486e93b80bf0e0c6aa81da74bdfdbec5
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-05-20 16:24:20 -0500

    pointers

--- c/41_pointers.c
+++ c/41_pointers.c
@@ -27,6 +27,32 @@ int numStudents = 12;
 students = calloc(numStudents, sizeof(*students));
 printf("%ld", numStudents * sizeof(*students)); // 48 bytes
 
+printf("consider \n");
+printf("=====================");
+printf("=====================");
+printf("=====================\n");
+
+int j,k;
+k = 2;
+j = 7;
+k = j;
+
+int *ptr;
+printf("%p\n",ptr);
+printf("%d\n",*ptr);
+
+ptr = &k;
+
+printf("\n");
+printf("%p\n",ptr);
+printf("%d\n",*ptr);
+
+printf("\n");
+printf("j has the values %d and is stored at %p\n", j,(void *)&j);
+printf("k has the value %d and is stored at %p\n",k, (void *)&k);
+printf("ptr has the value %p and is stored at %p\n", ptr, (void *)&ptr);
+printf("*ptr has the value %d \n", *ptr);
+
 	return 0;
 
 }
