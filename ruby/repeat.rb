c14b6aa5decb4475cb9c39e0c0e47e0d67904180
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-06-08 21:43:03 -0500

    iterator

--- ruby/repeat.rb
+++ ruby/repeat.rb
@@ -1107,6 +1107,20 @@ puts a
 
 
 
+class Array
+  def inject(n)
+     each { |value| n = yield(n, value) }
+     n
+  end
+  def sum
+    inject(0) { |n, value| n + value }
+  end
+  def product
+    inject(1) { |n, value| n * value }
+  end
+end
+[ 1, 2, 3, 4, 5 ].sum	»	15
+[ 1, 2, 3, 4, 5 ].product	»	120
 
 
 
