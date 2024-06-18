9020cc5355149edbfb8aadeb2a6fec5a5e118df5
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-06-18 10:05:52 -0500

    rest-spread

---
+++ javascript/rest-vs-spread.js
@@ -0,0 +1,22 @@
+console.log("====================== rest syntax ======================")
+//  1. receiving data: rest syntax
+
+function myFunc(first,...remaining){
+	return {first,remaining}	
+}
+console.log( myFunc(1,2,3,4,5,6,7) )   // 	{ first: 1, remaining: [ 2, 3, 4, 5, 6, 7 ] }
+
+
+const[head,...tail] = ["a","b","c"]
+console.log( head )   // "a"	
+console.log( tail )   // ["b","c"] 	
+
+
+// we can use it in object-destructuring:
+const {first:f, ...remaining} = {first: 'Jane', last: 'Doe', age: 43};
+
+
+console.log( f )   // "a"	
+console.log( remaining )   // "a"	
+const {...rema} = {first: 'Jane', last: 'Doe', age: 43,name:"wallo",age_:23,ae_:23,ge_:23};
+console.log( rema )   // "a"	
